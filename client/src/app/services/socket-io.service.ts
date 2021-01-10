cc3bbc6f993677cc18c2b6c24031a903b2fd12a1
Norbert Sanpruch <norbert.sanpruch@miquido.com>
2021-01-10 19:04:25 +0100

    feat(web socket): establish simple connection between client side and web socket server

---
+++ client/src/app/services/socket-io.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketIoService {
+  constructor() {}
+
+  private readonly uri: string = 'http://localhost:3000/';
+  socket: any = io.io(this.uri);
+
+  listen(eventName: string) {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data) => {
+        subscriber.next(data);
+      });
+    });
+  }
+
+  emit(eventName: string, data: any) {
+    this.socket.emit(eventName, data);
+  }
+}
